66e88f08038a877ffa01a0d1d86cd42ba9c2b7a9
beatricecourage <bea_courage@hotmail.co.uk>
2019-01-18 14:42:19 +0000

    First test passing, gives fizz with multiples of 3

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -1,1 +1,15 @@
+describe('Fizzbuzz', function(){
 
+  var fizzbuzz;
+
+  beforeEach(function(){
+    fizzbuzz = new Fizzbuzz();
+  });
+
+  describe('Multiples of 3', function() {
+    it('Gives fizz with 3', function(){
+      expect(fizzbuzz.play(3)).toEqual('Fizz');
+    });
+  });
+
+});
--- src/Player.js
+++ src/Player.js
@@ -1,2 +1,17 @@
-function Fizzbuzz () {
+function Fizzbuzz() {
 }
+
+Fizzbuzz.prototype.divisibleby = function(divisor, number) {
+  return number % divisor === 0;
+}
+
+Fizzbuzz.prototype.play = function(number){
+  if (this.divisibleby(3,number)){
+    return 'Fizz';
+  } else {
+    return number
+  }
+}
+
+
+// created a Fizzbuzz object with a method play(number)
